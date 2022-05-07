444a20ada1eb7549a33b599a3519e8ac51e4004c
Pato <sy2470@nyu.edu>
2022-05-07 21:59:28 +0800

    :poultry_leg:  一些常识

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -35,7 +35,7 @@ public class MeshGenerator : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         UpdateMesh();
     }
--- Assets/Scripts/leafTornadoTrigger.cs
+++ Assets/Scripts/leafTornadoTrigger.cs
@@ -7,12 +7,16 @@ public class leafTornadoTrigger : MonoBehaviour
     
     public float leafTornadoStartTime;
     private float _leafTornadoStartTimer;
+
+    public float leafTornadoEndTime;
+    private float _leafTornadoEndTimer;
     
     // Start is called before the first frame update
     void Start()
     {
         transform.GetChild(0).gameObject.SetActive(false);
         _leafTornadoStartTimer = leafTornadoStartTime;
+        _leafTornadoEndTimer = leafTornadoEndTime;
     }
 
     // Update is called once per frame
@@ -26,5 +30,15 @@ public class leafTornadoTrigger : MonoBehaviour
         {
             _leafTornadoStartTimer -= Time.deltaTime;
         }
+        
+        if (_leafTornadoEndTimer < 0)
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+        else
+        {
+            _leafTornadoEndTimer -= Time.deltaTime;
+        }
+        
     }
 }
